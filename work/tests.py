41541f6a9c6c1b343ff5071a5b5c6512967fea04
tsokac2 <tsokac2@gmail.com>
2021-07-09 17:45:20 +0100

    Update views.py file in the life app root folder

--- work/tests.py
+++ work/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestViews(TestCase):
+    def test_work_page(self):
+        """ Test Work page renders correct page """
+        response = self.client.get('/work')
+        self.assertEqual(response.status_code, 200)
+        self. assertTemplateUsed(response, 'work/work.html')
--- work/views.py
+++ work/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """ A view to return the Work page """
+
+    return render(request, 'work/work.html')
+
+

78949aca1397509143d75d6c46553eecf833ba96
tsokac2 <tsokac2@gmail.com>
2021-07-09 17:55:49 +0100

    Update views.py file in the life app root folder

--- life/tests.py
+++ life/tests.py
@@ -1,1 +1,8 @@
 from django.test import TestCase
+
+class TestViews(TestCase):
+    def test_life_page(self):
+        """ Test Life page renders correct page """
+        response = self.client.get('/life')
+        self.assertEqual(response.status_code, 200)
+        self. assertTemplateUsed(response, 'life/life.html')
--- life/views.py
+++ life/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+
+
+def index(request):
+    """ A view to return the life section page """
+
+    return render(request, 'life/life.html')

c458368d49977fc5bea45ccca6ad202710f47449
tsokac2 <tsokac2@gmail.com>
2021-07-09 17:57:51 +0100

    Update urls.py file in life app root with URL's

--- life/views.py
+++ life/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    """ A view to return the life section page """
+    """ A view to return the Life page """
 
     return render(request, 'life/life.html')

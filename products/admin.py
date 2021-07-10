d9a476c9751674f09029122a9af5be44a1d379ed
tsokac2 <tsokac2@gmail.com>
2021-07-10 15:01:45 +0100

    Register products models to admin server

--- products/admin.py
+++ products/admin.py
@@ -1,1 +1,23 @@
 from django.contrib import admin
+from .models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+    )
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)

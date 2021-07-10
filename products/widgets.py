c0fdb9d1f57ccbb09acc0b0cb91c22c21d27e2d0
tsokac2 <tsokac2@gmail.com>
2021-07-10 15:47:58 +0100

    Create custom widgets.py file

---
+++ products/widgets.py
@@ -0,0 +1,26 @@
+from django.forms.widgets import ClearableFileInput
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomClearableFileInput_1(ClearableFileInput):
+    clear_checkbox_label = _('Remove')
+    initial_text = _('Current Image')
+    input_text = _('')
+    template_name =
+    'products/custom_widget_templates/custom_clearable_file_input_1.html'
+
+
+class CustomClearableFileInput_2(ClearableFileInput):
+    clear_checkbox_label = _('Remove')
+    initial_text = _('Current Image')
+    input_text = _('')
+    template_name =
+    'products/custom_widget_templates/custom_clearable_file_input_2.html'
+
+
+class CustomClearableFileInput_3(ClearableFileInput):
+    clear_checkbox_label = _('Remove')
+    initial_text = _('Current Image')
+    input_text = _('')
+    template_name =
+    'products/custom_widget_templates/custom_clearable_file_input_3.html'

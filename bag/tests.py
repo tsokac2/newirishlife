42ea95687608d9d61e3b668c07dea8398b4fc840
tsokac2 <tsokac2@gmail.com>
2021-07-10 20:15:28 +0100

    Change unit test in bag app

--- bag/tests.py
+++ bag/tests.py
@@ -1,1 +1,9 @@
 from django.test import TestCase
+
+
+class TestViews(TestCase):
+    def test_shoping_bag_page(self):
+        """ Test shoping bag page renders correct page """
+        response = self.client.get('/bag/')
+        self.assertEqual(response.status_code, 200)
+        self. assertTemplateUsed(response, 'bag/bag.html')

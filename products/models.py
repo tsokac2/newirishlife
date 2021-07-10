57d3202289383f52528a9bbd46fb844058b518a8
tsokac2 <tsokac2@gmail.com>
2021-07-10 15:17:15 +0100

    Create products models for products app

--- products/models.py
+++ products/models.py
@@ -1,2 +1,33 @@
 from django.db import models
 
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    sale_price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=2, decimal_places=1, null=True, blank=True)
+    image_1 = models.ImageField(null=True, blank=True)
+    image_2 = models.ImageField(null=True, blank=True)
+    image_3 = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name

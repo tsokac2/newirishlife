a8448e43087acc7450896f6ad046847e9cecedd4
tsokac2 <tsokac2@gmail.com>
2021-07-10 15:19:14 +0100

    Create unit test for products app

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Product
+
+
+class TestViews(TestCase):
+
+    def test_products_page(self):
+        """ Test Products page renders correct page """
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self. assertTemplateUsed(response, 'products/products.html')
 
 

7867ed028ebb428eeaac4f319dcb252da4766113
tsokac2 <tsokac2@gmail.com>
2021-07-11 17:02:17 +0100

    Create unit test for profiles app

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,2 +1,21 @@
 from django.test import TestCase
+from .forms import UserProfileForm
 
+
+class TestViews(TestCase):
+    def test_user_profile_page(self):
+        """ Test User Profile redirects when user is logged out """
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 302)
+
+
+class TestUserForm(TestCase):
+    """ Test to test that form field is valid """
+    def test_edit_profile_form(self):
+        form = UserProfileForm(
+            {
+                'full_name': 'John Snow',
+                'phone_number': '+38508555566655',
+                'city': 'Winterfell'
+                })
+        self.assertTrue(form.is_valid())

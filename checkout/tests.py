7964ee7aa2fb30b8576af646fba36028d1e8886b
tsokac2 <tsokac2@gmail.com>
2021-07-11 12:32:32 +0100

    Create unit tests for checkout app

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,1 +1,9 @@
 from django.test import TestCase
+from .forms import OrderForm
+
+
+class TestViews(TestCase):
+    def test_checkout_page_redirects_when_logged_out(self):
+        """ Test Checkout redirects when user is logged out """
+        response = self.client.get('/checkout/')
+        self.assertEqual(response.status_code, 302)

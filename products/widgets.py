3472d3698c192da90b455851bcb0d82630804e35
tsokac2 <tsokac2@gmail.com>
2021-07-11 17:16:22 +0100

    Fix code layout for PEP8 validation

--- products/widgets.py
+++ products/widgets.py
@@ -6,18 +6,21 @@ class CustomClearableFileInput_1(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('')
-    template_name = 'products/custom_widget_templates/custom_clearable_file_input_1.html'
+    template_name = (
+        'products/custom_widget_templates/custom_clearable_file_input_1.html')
 
 
 class CustomClearableFileInput_2(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('')
-    template_name = 'products/custom_widget_templates/custom_clearable_file_input_2.html'
+    template_name = (
+        'products/custom_widget_templates/custom_clearable_file_input_2.html')
 
 
 class CustomClearableFileInput_3(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('')
-    template_name = 'products/custom_widget_templates/custom_clearable_file_input_3.html'
+    template_name = (
+        'products/custom_widget_templates/custom_clearable_file_input_3.html')
